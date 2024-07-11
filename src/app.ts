31f43f3aa06ed4784def1b3de14c3d31e9ebb727
RaggedyRach <rachaelnolan@gmail.com>
2024-07-11 12:29:06 +0100

    adding authorisation into pages

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,8 @@ import session from "express-session";
 import { getAllDatabases } from "./controllers/TestController";
 import { getAllEmployees, getEmployeeForm, postEmployeeForm } from "./controllers/employeeController";
 import { getLoginForm, postLoginForm } from "./controllers/AuthController";
+import { UserRole } from "./models/JwtToken";
+import { allowRoles } from "./middleware/AuthMiddleware";
 
 
 const app = express();
@@ -32,9 +34,8 @@ app.listen(3000, () => {
     console.log('Server started on port 3000');
 });
 
-app.get('/', getAllDatabases);
-app.get('/employees', getAllEmployees);
-app.get('/employeeForm', getEmployeeForm);
-app.post('/employeeForm', postEmployeeForm);
+app.get('/employees', allowRoles([UserRole.HR]), getAllEmployees);
+app.get('/employeeForm', allowRoles([UserRole.HR]), getEmployeeForm);
+app.post('/employeeForm', allowRoles([UserRole.HR]), postEmployeeForm);
 app.get('/loginForm', getLoginForm);
 app.post('/loginForm', postLoginForm);
