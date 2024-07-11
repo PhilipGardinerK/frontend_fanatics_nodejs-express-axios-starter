bc02a499011670be6dd46a77c6a84b88e6cf8f92
Ashna Abraham <ashna.abraham@kainos.com>
2024-07-11 14:19:04 +0100

    changing from emp to delivery emp

--- src/services/employeeService.ts
+++ src/services/employeeService.ts
@@ -14,7 +14,7 @@ import { employeeRequest } from "../models/employeeRequest";
 }
     export const createEmployee = async (employee: employeeRequest): Promise<Number> => {
         try{
-           const response: AxiosResponse = await axios.post("http://localhost:8080/api/employees", employee);
+           const response: AxiosResponse = await axios.post("http://localhost:8080/api/employees/delivery", employee);
            return response.data;
         } catch (e) {
            console.log(e);
